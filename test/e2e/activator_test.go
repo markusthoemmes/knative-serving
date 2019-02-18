33a1c5b04212a5e5794304305dc8f062aef23145
Vadim Raskin <vadimr@de.ibm.com>
2019-02-18 17:58:33 +0100

    Inline analyze func, print out stats in a separate go routine

--- test/e2e/activator_test.go
+++ test/e2e/activator_test.go
@@ -24,21 +24,21 @@ import (
 	"time"
 	"sync/atomic"
 	"net/http"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/knative/pkg/test/logging"
 	"github.com/knative/pkg/test/spoof"
 	"github.com/knative/serving/test"
 	"github.com/knative/serving/pkg/apis/serving/v1alpha1"
 	pkgTest "github.com/knative/pkg/test"
-	"golang.org/x/sync/errgroup"
 )
 
 // TestActivatorOverload makes sure that activator can handle the load when scaling from 0.
 // We need to add a similar test for the User pod overload once the second part of overload handling is done.
 func TestActivatorOverload(t *testing.T) {
 	var (
-		clients *test.Clients
-		logger  *logging.BaseLogger
+		logger  = logging.GetContextLogger(t.Name())
+		clients = Setup(t)
 	)
 	const (
 		// The number of concurrent requests to hit the activator with.
@@ -50,10 +50,8 @@ func TestActivatorOverload(t *testing.T) {
 		// How long the service will process the request in ms.
 		serviceSleep = 300
 	)
-	logger = logging.GetContextLogger(t.Name())
-	clients = Setup(t)
 
-	helloWorldNames := test.ResourceNames{
+	names := test.ResourceNames{
 		Service: test.AppendRandomString(configName, logger),
 		Image:   "observed-concurrency",
 	}
@@ -67,12 +65,12 @@ func TestActivatorOverload(t *testing.T) {
 		service.Spec.RunLatest.Configuration.RevisionTemplate.Annotations["autoscaling.knative.dev/maxScale"] = "10"
 	}
 
-	test.CleanupOnInterrupt(func() { TearDown(clients, helloWorldNames, logger) }, logger)
-	defer TearDown(clients, helloWorldNames, logger)
+	test.CleanupOnInterrupt(func() { TearDown(clients, names, logger) }, logger)
+	defer TearDown(clients, names, logger)
 
 	// Create a service with concurrency 1 that could sleep for N ms.
 	// Limit its maxScale to 10 containers, wait for the service to scale down and hit it with concurrent requests.
-	resources, err := test.CreateRunLatestServiceReady(logger, clients, &helloWorldNames, &configOptions, fopt)
+	resources, err := test.CreateRunLatestServiceReady(logger, clients, &names, &configOptions, fopt)
 	if err != nil {
 		t.Fatalf("Unable to create resources: %v", err)
 	}
@@ -99,82 +97,79 @@ func TestActivatorOverload(t *testing.T) {
 
 	sendRequests(client, url, concurrency, responseChannel, timeout, logger, t)
 
-	analyseResponses(responseChannel, concurrency, timeout, t)
 }
 
 func sendRequests(client *spoof.SpoofingClient, url string, concurrency int, resChannel chan *spoof.Response, timeout time.Duration, logger *logging.BaseLogger, t *testing.T) {
+	t.Helper()
 	var (
-		group     errgroup.Group
-		responses int32
+		group        errgroup.Group
+		responses    int32
+		wantResponse = http.StatusOK
 	)
-	doneChan := make(chan struct{})
 	timeoutChan := time.After(timeout)
 	errChan := make(chan error)
 
 	// Send out the requests asynchronously and wait for them to finish.
+	logger.Info("Starting to send out the requests")
+
+	// Print out stats.
 	go func() {
-		logger.Info("Starting to send out the requests")
-		// Send requests async and wait for the responses.
-		for i := 0; i < concurrency; i++ {
-			group.Go(func() error {
-				req, err := http.NewRequest(http.MethodGet, url, nil)
-				if err != nil {
-					return fmt.Errorf("error creating http request: %v", err)
-				}
-				res, err := client.Do(req)
-				if err != nil {
-					return fmt.Errorf("unexpected error sending a request, %v\n", err)
-				}
-				atomic.AddInt32(&responses, 1)
-				resChannel <- res
-				return nil
-			})
+		for {
+			select {
+			case <-time.Tick(5 * time.Second):
+				logger.Infof("Received responses: %d", atomic.LoadInt32(&responses))
+			case <-timeoutChan:
+				return
+			}
 		}
-		if err := group.Wait(); err != nil {
-			errChan <- fmt.Errorf("unexpected error making requests against activator: %v", err)
-		}
-		logger.Info("Done sending out requests")
-		close(doneChan)
 	}()
+
+	// Send requests async and wait for the responses.
+	for i := 0; i < concurrency; i++ {
+		group.Go(func() error {
+			req, err := http.NewRequest(http.MethodGet, url, nil)
+			if err != nil {
+				return fmt.Errorf("error creating http request: %v", err)
+			}
+			res, err := client.Do(req)
+			if err != nil {
+				return fmt.Errorf("unexpected error sending a request, %v\n", err)
+			}
+			atomic.AddInt32(&responses, 1)
+			resChannel <- res
+			return nil
+		})
+	}
+	if err := group.Wait(); err != nil {
+		errChan <- fmt.Errorf("unexpected error making requests against activator: %v", err)
+	}
+	logger.Info("Done sending out requests")
+	close(resChannel)
 
 	logger.Info("Waiting for all requests to finish")
 
-done:
-	for {
-		select {
-		case <-doneChan:
-			break done
-		case <-time.Tick(5 * time.Second):
-			logger.Infof("Received responses: %d", atomic.LoadInt32(&responses))
-		case err := <-errChan:
-			t.Fatalf("Error happened while waiting for the responses: %v", err)
-		case <-timeoutChan:
-			t.Fatalf("Timed out after %v while collecting responses, collected responses %d, out of %d", timeout, atomic.LoadInt32(&responses), concurrency)
+	select {
+	case resp, ok := <-resChannel:
+		if !ok {
+			// The channel is closed, no more responses.
+			break
 		}
+		if resp != nil {
+			if resp.StatusCode != wantResponse {
+				t.Errorf("Response code = %d, want: %d", resp.StatusCode, wantResponse)
+			}
+		} else {
+			t.Errorf("No response code received for the request")
+		}
+	case err := <-errChan:
+		t.Fatalf("Error happened while waiting for the responses: %v", err)
+	case <-timeoutChan:
+		t.Fatalf("Timed out after %v while collecting responses, collected responses %d, out of %d", timeout, atomic.LoadInt32(&responses), concurrency)
 	}
+
 	logger.Info("Finished waiting for the responses")
 
 	if atomic.LoadInt32(&responses) != int32(concurrency) {
 		t.Fatalf("Number of activator responses = %d, want: %d", responses, concurrency)
-	}
-}
-
-func analyseResponses(respChan chan *spoof.Response, total int, timeout time.Duration, t *testing.T) {
-	t.Helper()
-	timeoutChan := time.After(timeout)
-	wantResponse := http.StatusOK
-	for i := 0; i < total; i++ {
-		select {
-		case resp := <-respChan:
-			if resp != nil {
-				if resp.StatusCode != wantResponse {
-					t.Errorf("Response code = %d, want: %d", resp.StatusCode, wantResponse)
-				}
-			} else {
-				t.Errorf("No response code received for the request")
-			}
-		case <-timeoutChan:
-			t.Fatalf("timed out after %d while analyzing the responses", timeout)
-		}
 	}
 }
